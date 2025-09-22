48472795c14961eb84362193611d0995d7152467
Salma <salmaakagou1@gmail.com>
2025-09-22 15:16:23 +0100

    ajout ConvertibleInterface.php

---
+++ Prototype/src/ConvertibleInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+namespace App;
+interface ConvertibleInterface{
+    public function toDecimal() : int;
+    public function toBinary() : string;
+    public function toHex() : string;
+}
