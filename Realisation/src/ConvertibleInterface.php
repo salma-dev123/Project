4cffbf9c01043d8429a929daefd032521ae19de8
Salma <salmaakagou1@gmail.com>
2025-09-24 22:30:40 +0100

    ConvertibleInterface.php

---
+++ Realisation/src/ConvertibleInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+interface ConvertibleInterface {
+    public function toDecimal(): int;
+    public function toBinary(): string;
+    public function toHex(): string;
+}
